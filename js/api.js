9bca8ba94457b9d98bc3d18de0eea0f959545372
DaisukeChiba <d_chiba@mac.com>
2017-12-10 17:23:46 +0900

    Clean

--- js/api.js
+++ js/api.js
@@ -1,6 +1,6 @@
 // Cognitoユーザープールオーソライザーを使用した
 // 認証付きAPI Gatewayの呼び出し(外部API)
-function apicall1(){
+function apicall(){
     var req = new XMLHttpRequest();
     req.onreadystatechange = function(){
         if((req.readyState == 4) && (req.status == 200)){
@@ -11,24 +11,7 @@ function apicall1(){
             document.getElementById('book_result').innerHTML = reqHtml;
         }
     }
-    req.open('GET','https://cwi5f3nkv9.execute-api.ap-northeast-1.amazonaws.com/prod?isbn=' +  + document.getElementById('isbn').value,false);
-    cognitoUser.getSession(function(err, result) {
-        req.setRequestHeader('Authorization', result.getIdToken().jwtToken);
-    });
-    req.send(null);
-}
-
-// Cognitoユーザープールオーソライザーを使用した
-// 認証付きAPI Gatewayの呼び出し(Node-RED API)
-function apicall2(){
-    var req = new XMLHttpRequest();
-    req.onreadystatechange = function(){
-        if((req.readyState == 4) && (req.status == 200)){
-            // Node-REDで編集されたHTMLを画面に表示
-            document.getElementById('program_result').innerHTML = req.response;
-        }
-    }
-    req.open('GET','https://5hn8m6suf8.execute-api.ap-northeast-1.amazonaws.com/prod?area=130&genre=' + document.getElementById('genre').value,false);
+    req.open('GET','https://xxxxxxxxxx.execute-api.ap-northeast-1.amazonaws.com/prod?isbn=' +  + document.getElementById('isbn').value,false);
     cognitoUser.getSession(function(err, result) {
         req.setRequestHeader('Authorization', result.getIdToken().jwtToken);
     });
