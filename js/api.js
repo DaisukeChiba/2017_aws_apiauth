4b2d09e1f8905489c1d86a5772b9b190560c6a78
DaisukeChiba <d_chiba@mac.com>
2017-12-10 17:28:28 +0900

    Clean

--- js/api.js
+++ js/api.js
@@ -5,13 +5,18 @@ function apicall(){
     req.onreadystatechange = function(){
         if((req.readyState == 4) && (req.status == 200)){
             // Google Books APIの返却結果から最初の1冊目の書籍情報を画面に表示
+            // APIから返却された文字列値をJSON形式に変換
             var book = JSON.parse(req.response);
+            // 画面に表示するHTMLを編集
             var reqHtml = '<p>' + 'タイトル: ' + book.items[0].volumeInfo.title + '<br />';
             reqHtml = reqHtml + '説明: ' + book.items[0].volumeInfo.description + '</p>';
+            // 画面表示
             document.getElementById('book_result').innerHTML = reqHtml;
         }
     }
-    req.open('GET','https://xxxxxxxxxx.execute-api.ap-northeast-1.amazonaws.com/prod?isbn=' +  + document.getElementById('isbn').value,false);
+    // API Gatewayの呼び出し
+    req.open('GET', 'https://xxxxxxxxxx.execute-api.ap-northeast-1.amazonaws.com/prod?isbn=' + document.getElementById('isbn').value, false);
+    // Cognito認証からトークンを取得し、リクエストヘッダのAuthorizationにセット
     cognitoUser.getSession(function(err, result) {
         req.setRequestHeader('Authorization', result.getIdToken().jwtToken);
     });
